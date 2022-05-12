4e04563dc17db194de7e5f90789426a085294503
david-j-kim <david.jaekang.kim@gmail.com>
2022-05-12 15:24:33 -0400

    initialize industry project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//global
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sample from './pages/Sample/Sample';
+import Navigation from './components/Navigation/Navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navigation />
+
+        <Switch>
+          <Route path='/' exact component={Sample} />
+
+
+
+        </Switch>
+
+    </Router>
   );
 }
 
---
+++ src/pages/Sample/Sample.js
@@ -0,0 +1,39 @@
+import './Sample.scss';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+class Sample extends Component {
+
+    state = {
+        sample: []
+    }
+    
+    componentDidMount() {
+        axios
+        .get(`${API_URL}/sample`)
+        .then((res) => {
+            this.setState({sample: res.data})
+        });
+      }
+
+    render() {
+
+        const {sample} = this.state;
+
+        if(!sample[0]) {
+            return;
+        }
+
+        return (
+
+            <div>{`Test data returned from server: ${sample[0].name}`}</div>
+
+        );
+
+    }
+
+}
+
+export default Sample;
